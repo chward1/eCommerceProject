6193ad2a5532b63bd8872773f1fe80be5b2baf24
chward1 <christina.harney@gmail.com>
2021-04-01 22:11:39 -0700

    Added server to product list

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,5 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
 
 export default function ProductList() {
-    return <>ProductList</>
+    const [products, setProducts] = useState([])
+
+    const loadData = async () => {
+        const result = await axios('http://localhost:3003/products')
+        setProducts(result.data.result)
+    }
+    useEffect(() => {
+        loadData()
+        return () => { }
+    }, []);
+
+    return (
+        <ul>
+            {products.map(product => (
+                <li key={product.id}>
+                    <p>{product.name}</p>
+                </li>
+            ))}
+        </ul>
+    );
 }
